f68caeb216f3b53c56d3fc1537ce67788b7a8d88
Brian Done <briguy2078@gmail.com>
2021-12-07 15:48:59 -0500

    added .gitignore, json, and inquirer function for name

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
+const inquirer = require('inquirer');
 // Include File System Core Module to access its methods
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
 
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-
-const [name, github] = profileDataArgs;
+// const pageHTML = generatePage(name, github);
 
 // 1st Argument = File Name, 2nd Argument = Data being Written
 // 3rd Argument = Error Handling Callback Function
-fs.writeFile('index.html',generatePage(name, github), err => {
-    if (err) throw err;
+// fs.writeFile('index.html', pageHTML, err => {
+//     if (err) throw err;
 
-    console.log('Portfolio complete! Check out index.html to see the output!')
-});
+//     console.log('Portfolio complete! Check out index.html to see the output!')
+// });
 
-
+inquirer
+.prompt([
+    {
+        type: 'input',
+        name: 'name', // key and user input
+        message: 'What is your name?'
+    }
+])
+.then(answers => console.log(answers)); // answer object
 
